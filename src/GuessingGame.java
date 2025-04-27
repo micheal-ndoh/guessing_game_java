24ddbdda8ff7c728aa34a1f43783f153a26251ff
micheal-ndoh <michaelndoh9@gmail.com>
2025-04-27 21:30:45 +0100

    Add GuessingGame implementation with multiple difficulty modes

---
+++ src/GuessingGame.java
@@ -0,0 +1,114 @@
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class GuessingGame {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("|------------------------------------|");
+        System.out.println("| ⚜️ Welcome to the Guessing Game ⚜️ |");
+        System.out.println("|------------------------------------|");
+
+        while (true) {
+            int mode = -1;
+            boolean validInput = false;
+
+            while (!validInput) {
+                System.out.println("\nSelect a mode:");
+                System.out.println("0. EASY");
+                System.out.println("1. NORMAL");
+                System.out.println("2. HARD");
+                System.out.println("3. SUPER HARD");
+                System.out.println("4. Win twice and receive 1k Mode");
+                System.out.print("Enter MODE: ");
+
+                try {
+                    mode = scanner.nextInt();
+                    if (mode >= 0 && mode <= 4) {
+                        validInput = true;
+                    } else {
+                        System.out.println("Invalid mode. Please enter a number between 0 and 4.");
+                    }
+                } catch (InputMismatchException e) {
+                    System.out.println("Invalid input. Please enter a number between 0 and 4.");
+                    scanner.next();
+                }
+            }
+
+            int range = 0;
+            int tries = 3;
+
+            switch (mode) {
+                case 0:
+                    System.out.println("😃EASY MODE😃");
+                    range = 10;
+                    break;
+                case 1:
+                    System.out.println("NORMAL MODE");
+                    range = 50;
+                    break;
+                case 2:
+                    System.out.println("HARD MODE");
+                    range = 100;
+                    break;
+                case 3:
+                    System.out.println("SUPER HARD MODE");
+                    range = 500;
+                    break;
+                case 4:
+                    System.out.println("Win twice in a row and receive 5k Mode");
+                    range = 1000;
+                    break;
+            }
+
+            int number = (int) (Math.random() * range);
+            System.out.println("\nI'm thinking 🤔 of a number between 0 and " + range + ". Can you guess it?");
+            System.out.println("-------------------------------------------------------------------------");
+
+            while (tries > 0) {
+                System.out.print("\nEnter your guess: ");
+                int guess = scanner.nextInt();
+
+                if (guess == number) {
+
+                    System.out.println("\n🎋🎋🎋🎋🎋🎋🎋🎋🎋🎋🎋🎋🎋🎋🎋🎋🎋🎋🎋🎋");
+                    System.out.println("🎋  You won ☺️🎉 you recieve a star ⭐ 🎋");
+                    System.out.println("🎋🎋🎋🎋🎋🎋🎋🎋🎋🎋🎋🎋🎋🎋🎋🎋🎋🎋🎋🎋");
+                    break;
+                } else if (guess < number) {
+                    System.out.println("\n🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥");
+                    System.out.println("You missed, The random number is greater than " + guess + ".");
+                    System.out.println("🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥");
+                } else {
+                    System.out.println("🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥");
+                    System.out.println("You missed!The random number is lesser than " + guess + ".");
+                    System.out.println("🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥");
+                }
+
+                tries--;
+                if (tries > 0) {
+                    System.out.println("You have " + tries + " tries left. Please try again!");
+                }
+            }
+
+            if (tries == 0) {
+                System.out.println("\n🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥");
+                System.out.println(" You lose 😭 The number was " + number + ".");
+                System.out.println("🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥");
+
+            }
+            System.out.println("\n🎆🎆🎆🎆🎆🎆🎆🎆🎆🎆🎆🎆🎆🎆🎆🎆🎆🎆🎆🎆🎆🎆🎆🎆🎆🎆🎆🎆🎆");
+            System.out.println("Press Enter key to restart or any other key to exit:");
+            System.out.println("🎆🎆🎆🎆🎆🎆🎆🎆🎆🎆🎆🎆🎆🎆🎆🎆🎆🎆🎆🎆🎆🎆🎆🎆🎆🎆🎆🎆🎆");
+            scanner.nextLine();
+            String restart = scanner.nextLine();
+            if (!restart.isEmpty()) {
+                System.out.println("Thanks for playing Goodbye ");
+                break;
+            }
+        }
+
+        scanner.close();
+    }
+}
