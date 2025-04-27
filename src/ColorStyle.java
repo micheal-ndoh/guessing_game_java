93fa7ffee4a8a0722c3148720937bf7157500444
micheal-ndoh <michaelndoh9@gmail.com>
2025-04-27 21:55:24 +0100

    Add ColorStyle class for ANSI color and style formatting

---
+++ src/ColorStyle.java
@@ -0,0 +1,18 @@
+
+public class ColorStyle {
+
+    class Color {
+
+        static String red = "\u001B[31m";
+        static String green = "\u001B[32m";
+        static String blue = "\u001B[34m";
+        static String reset = "\u001B[0m";
+
+    }
+
+    class Style {
+
+        static String bold = "\u001B[1m";
+        static String reset = "\u001B[0m";
+    }
+}
